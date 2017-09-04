ec24bc4b6016df74874d9ed91714fb1b118fc2c0
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2017-09-04 17:23:16 -0300

    Comminting Initial Project Structure

---
+++ WatchGroupChallenge/WatchGroupChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchGroupChallenge
+//
+//  Created by Gabriel Rodrigues on 04/09/17.
+//  Copyright © 2017 Gabriel Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
